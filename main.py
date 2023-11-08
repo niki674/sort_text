b2d3e08166c4942a47c9e655538adac851726fee
Nikita Kalchenko <nikitakalcenko9@gmail.com>
2023-11-08 14:19:15 +0300

    start project

--- main.py
+++ main.py
@@ -7,15 +7,26 @@ for i in punct:
     t=t.replace(i, '')
 
 ts = t.split()
+print('Всего слов:', len(ts))
 tset = set(ts)
 ts1 = list(tset)
 tmax=''
+print(len(ts1))
 tmin=ts1[0]
+
 for i in ts1:
     if len(i)>len(tmax):
         tmax=i
     if len(i)<len(tmin):
         tmin=i
+
+tw = {}
+for i in t:
+    if i in tw:
+        tw[i] += 1
+    else:
+        tw[i] = 1
+
 tc={}
 for i in ts:
     if i not in tc:
@@ -34,4 +45,9 @@ print("Самое длинное слово:", tmax)
 print("Самое короткое слово:", tmin)
 print("Частота слов:")
 for word, frequency in tc.items():
+    print(f"{word}: {frequency}")
+
+print('Всего букв:', len(t))
+print("Частота букв:")
+for word, frequency in tw.items():
     print(f"{word}: {frequency}")
